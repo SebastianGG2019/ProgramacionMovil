41890995f020e266c4a3a61591aa23b628821900
cristian lopez cadena <cristian.lopez@grupomok.com>
2021-05-04 22:07:25 -0500

    Cambios en gastos

--- app/src/main/java/edu/papprecursos/proyectofinal/Home.java
+++ app/src/main/java/edu/papprecursos/proyectofinal/Home.java
@@ -1,14 +1,28 @@
 package edu.papprecursos.proyectofinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home  extends AppCompatActivity {
+
+    Button gastos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        gastos = (Button)findViewById(R.id.btnGastos);
+        gastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gastos = new Intent(Home.this, Gastos.class);
+                startActivity(gastos);
+            }
+        });
     }
 }
